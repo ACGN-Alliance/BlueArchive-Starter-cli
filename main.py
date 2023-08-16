0957202cee5f4f6ed98963ccd99cb9e24068c43b
MRSlouzk <MRSlouu@163.com>
2023-08-16 22:59:39 +0800

    :sparkles: 程序主体内容 :fire: 删除click模块

--- main.py
+++ main.py
@@ -1,8 +1,116 @@
-import click
+import sys, os, time
+from loguru import logger
+
+from utils import adb
 
 __version__ = "0.0.1"
 
-click.echo(f"欢迎使用BlueArchive-Starter-cli工具, 当前版本{__version__}")
-click.confirm("再次回车以继续", abort=True, default=True)
+device_now = ""
+adb_con = None  # adb类变量
+all_device_lst = {}
+port = 0
 
-click.echo("已结束")
+def _if_adb_exists():
+    # TODO 检测环境变量 + 子目录
+    if os.path.exists("adb.exe") or os.path.exists("adb"):
+        return True
+    else:
+        return False
+
+def menu():
+    print("\n" * 3)
+    if device_now:
+        print("当前设备: " + device_now + "端口: " + str(port))
+    else:
+        print("当前未连接设备")
+
+    print("1. 注意事项(必读)")
+    print("2. 扫描设备(TODO)")
+    print("3. 截图适配(TODO)")
+    print("4. 加载")
+    print("5. 运行脚本")
+    print("6. 退出")
+
+def notice():
+    pass
+
+def scan():
+    if not _if_adb_exists():
+        logger.error("未检测到adb可执行文件, 请将可执行文件放置于同目录下")
+
+    while True:
+        global adb_con, all_device_lst, device_now, port
+        adb_con = adb.ADB()
+        device_lst = adb_con.get_device_list()
+
+        print("1. 返回主菜单")
+        print("2. 重新扫描")
+        for i, device in enumerate(device_lst):
+            print(f"{i+3}. {device}")
+            all_device_lst[i+3] = device.split(" ")[0]
+
+        if len(device_lst) == 0:
+            print("\n未扫描到设备, 请尝试重新扫描")
+
+        device_num = input("请选择设备: ")
+        if device_num.isdigit():
+            device_num = int(device_num)
+        else:
+            print("请输入数字")
+            continue
+
+        if device_num == 1:
+            return
+        elif device_num == 2:
+            continue
+        else:
+            device_now = all_device_lst.get(device_num, "")
+            if not device_now:
+                print("请选择正确的设备")
+                continue
+
+            if "emulator" in device_now:
+                port = int(device_now.split("-")[1])
+            else:
+                port = 5555
+
+            print(f"已选择设备: {device_now}")
+            break
+
+def screenshot():
+    pass
+
+def load():
+    pass
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    print(f"欢迎使用BlueArchive-Starter-cli, 当前版本{__version__}, 作者: ACGN-Alliance, 交流群: 769521861")
+    # time.sleep(2)
+    while True:
+        menu()
+        mode = input("请选择模式: ")
+        if mode.isdigit():
+            mode = int(mode)
+        else:
+            print("请输入数字")
+            continue
+
+        if mode == 1:
+            notice()
+        elif mode == 2:
+            scan()
+        elif mode == 3:
+            screenshot()
+        elif mode == 4:
+            load()
+        elif mode == 5:
+            run()
+        elif mode == 6:
+            sys.exit(0)
+        else:
+            print("请选择正确的模式")
+            continue
---
+++ utils/adb.py
@@ -0,0 +1,210 @@
+import subprocess, os
+from pathlib import Path
+from io import BytesIO
+from PIL import Image, ImageChops
+from loguru import logger
+import time
+
+class ADB:
+    """
+    使用ADB (Android Debug Bridge) 与 Android 设备进行接口交互的类。
+    属性:
+        device_name (str): 设备的名称。
+        delay (int): 执行ADB命令后的延迟时间，单位为秒，默认为1秒。
+        adb_path (str): adb.exe可执行文件的路径。
+    """
+
+    def __init__(
+        self, adb_path: str = "", device_name: str = "", delay: int | float = 1
+    ):
+        """
+        初始化针对特定Android设备的ADB接口。
+        参数:
+            adb_path (str): adb 的路径
+            device_name (str): 设备的名称。
+            delay (int, optional): 执行ADB命令后的延迟时间，单位为秒。默认为1秒。
+        """
+        self.device_name = device_name
+        self.delay = delay
+        # self.adb_path = adb_path or self._find_adb()
+        if os.name == "nt":
+            self.adb_path = "adb.exe"
+        else:
+            self.adb_path = "./adb"
+        print(
+            subprocess.run(
+                [self.adb_path, "connect", device_name],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+        )
+        self.screen_width, self.screen_height = self._get_screen_resolution()
+
+    def _run_command(self, cmd: list[str]) -> str:
+        """执行一个ADB命令。"""
+        try:
+            result = subprocess.run(
+                [self.adb_path, "-s", self.device_name] + cmd,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            return result.stdout.decode("utf-8")
+        finally:
+            time.sleep(self.delay)
+
+    def _get_screen_resolution(self) -> tuple[int, int]:
+        """获取设备的屏幕分辨率."""
+        result = self._run_command(["shell", "wm", "size"])
+        output = result.strip().split()[-1]
+        width, height = output.split("x")
+        return int(width), int(height)
+
+    def _normalized_to_real_coordinates(self, x: float, y: float) -> tuple[int, int]:
+        """将0-100的浮点数坐标转换为实际的屏幕坐标."""
+        real_x = int(self.screen_width * x / 100)
+        real_y = int(self.screen_height * y / 100)
+        return real_x, real_y
+
+    def _real_to_normalized_coordinates(self, x: int, y: int) -> tuple[float, float]:
+        """将实际的屏幕坐标转换为0-100的浮点数坐标."""
+        normalized_x = (x / self.screen_width) * 100
+        normalized_y = (y / self.screen_height) * 100
+        return normalized_x, normalized_y
+
+    def click(self, x: float, y: float) -> str:
+        """
+        在给定的坐标上模拟设备屏幕上的点击操作。
+        参数:
+            x (float): 点击的x坐标(0-100表示)。
+            y (float): 点击的y坐标(0-100表示)。
+        """
+        real_x, real_y = self._normalized_to_real_coordinates(x, y)
+        return self._run_command(["shell", "input", "tap", str(real_x), str(real_y)])
+
+    def multi_click(self, x: float, y: float, count: int = 2) -> None:
+        """
+        在给定的坐标上模拟设备屏幕上的多次点击操作。
+        参数:
+            x (float): 点击的x坐标(0-100表示)。
+            y (float): 点击的y坐标(0-100表示)。
+            count (int, optional): 点击的次数。默认为2次。
+        """
+        for _ in range(count):
+            self.click(x, y)
+
+    def input_text(self, text: str) -> str:
+        """在设备上输入文字."""
+        return self._run_command(["shell", "input", "text", text])
+
+    def sleep(self, time_: int | float = 0) -> None:
+        """暂停指定的持续时间。"""
+        return time.sleep(time_ or self.delay)
+
+    def back(self) -> str:
+        """模拟设备上的返回按钮操作。"""
+        return self._run_command(["shell", "input", "keyevent", "KEYCODE_BACK"])
+
+    def screenshot(self, save_path: str) -> None:
+        """在设备上截屏并将其保存到主机机器的给定路径。"""
+        tmp_path = "/sdcard/screenshot.png"
+        self._run_command(["shell", "screencap", tmp_path])
+        self._run_command(["pull", tmp_path, save_path])
+        self._run_command(["shell", "rm", tmp_path])
+
+    def get_device_list(self) -> list:
+        """
+        获取设备列表
+        :return: tuple(序列号, 状态, 连接)
+        """
+        devices = []
+        out = self._run_command(["devices", "-l"])
+        for line in out.splitlines():
+            line = line.encode('utf-8')
+            if line.find("product") != -1:
+                devices.append(line)
+
+        return devices
+
+    def screenshot_region(
+        self,
+        x1: float,
+        y1: float,
+        x2: float,
+        y2: float,
+    ) -> Image.Image:
+        """
+        截取并保存设备上的一个特定区域的屏幕截图。
+        参数:
+            x1, y1, x2, y2 (float): 要截取的屏幕区域的坐标(0-100表示)。
+            save_path (str): 保存截图的路径。
+        """
+        full_screenshot_path = "temp_full_screenshot.png"
+        self.screenshot(full_screenshot_path)
+
+        with Image.open(full_screenshot_path) as img:
+            real_x1, real_y1 = self._normalized_to_real_coordinates(x1, y1)
+            real_x2, real_y2 = self._normalized_to_real_coordinates(x2, y2)
+            region = img.crop((real_x1, real_y1, real_x2, real_y2))
+
+        os.remove(full_screenshot_path)
+        return region
+
+    def compare_img(
+        self,
+        x1: float,
+        y1: float,
+        x2: float,
+        y2: float,
+        img: str | Path | BytesIO,
+        confidence: float = 0.9,
+    ) -> bool:
+        """
+        比较截图区域与指定图片的相似度。
+
+        参数:
+            x1, y1, x2, y2 (float): 要截取的屏幕区域的坐标(0-100表示)。
+            img (Union[str, Path, BytesIO]): 比较的图片路径或字节流。
+            confidence (float): 图像相似度的阈值。默认值为0.9。
+
+        返回:
+            bool: 如果两张图片的相似度超过给定的confidence，则返回True，否则返回False。
+        """
+        try:
+            # 截图并从区域获取Image对象
+            im: Image.Image = self.screenshot_region(x1, y1, x2, y2)
+            im_s: Image.Image = Image.open(img)
+
+            # 确保两个图像的尺寸一致
+            if im.size[0] / im.size[1] != im_s.size[0] / im_s.size[1]:
+                raise ValueError(
+                    "The aspect ratios of the two images are not the same!"
+                )
+
+            im_s = im_s.resize(im.size)
+
+            # 转换为RGB以确保图像格式一致
+            im = im.convert("RGB")
+            im_s = im_s.convert("RGB")
+
+            # 计算两个图像的差异
+            compare = ImageChops.difference(im, im_s)
+
+            # 判断对比合成图有多少RGB(0, 0, 0)像素点
+            # 如果有超过阈值的像素点，则认为两张图不相似
+            colors = compare.getcolors(maxcolors=16384)
+            all_count = len(im.getdata())
+
+            white_count = 0
+            for color in colors:
+                if color[1] == (0, 0, 0):
+                    white_count += color[0]
+                    break
+
+            now_confidence = white_count / all_count
+            logger.debug(f"matching rate {now_confidence}")
+
+            return now_confidence > confidence
+        except Exception as e:
+            logger.error(f"图片对比失败 {type(e)} {e}")
+            return False
+
