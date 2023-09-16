6c66095dc4a40d4f460615cabfd911069888bc78
MRSlouzk <MRSlouu@163.com>
2023-09-16 13:18:30 +0800

    :construction_worker: 更新nuitka打包参数

--- pack.bat
+++ pack.bat
@@ -1,1 +1,15 @@
-nuitka --standalone --lto=no --disable-plugin=multiprocessing --include-data-dir=platform-tools=platform-tools --include-data-dir=data/16_9=data/16_9 main.py
+nuitka ^
+    --standalone ^
+    --lto=no ^
+    --output-dir=build ^
+    --company-name="ACGN-Alliance" ^
+    --product-name="BlueArchive-Starter" ^
+    --disable-plugin=multiprocessing ^
+    --file-version="1.0.3" ^
+    --product-version="1.0.3" ^
+    --windows-file-description="BlueArchive Account tool" ^
+    --include-data-dir=platform-tools=platform-tools ^
+    --include-data-dir=data/16_9=data/16_9 ^
+    --copyright="Copyright ©ACGN-Alliance. All right reserved." ^
+    --remove-output ^
+    main.py ^
--- package.sh
+++ package.sh
@@ -7,11 +7,12 @@ python -m nuitka --mingw64 --standalone \
     --output-dir=build \
     --company-name="ACGN-Alliance" \
     --product-name="BlueArchive-Starter" \
-    --file-version="1.0.0" \
-    --product-version="1.0.0" \
+    --file-version="1.0.3" \
+    --product-version="1.0.3" \
     --file-description="BlueArchive Account tool" \
     --include-data-dir=platform-tools=platform-tools\
     --include-data-dir=data/16_9=data/16_9 \
+    --copyright="Copyright ©ACGN-Alliance. All right reserved." \
     --remove-output \
     --onefile \
     main.py
