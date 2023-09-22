e68182e4d99131898a92c84eeee7f8a835a1b348
MRSlou <79364553+MRSlouzk@users.noreply.github.com>
2023-09-22 21:12:53 +0800

    Update: 打包参数调整

--- pack.bat
+++ pack.bat
@@ -4,9 +4,10 @@ nuitka ^
     --output-dir=build ^
     --company-name="ACGN-Alliance" ^
     --product-name="BlueArchive-Starter" ^
+    --windows-icon-from-ico=bas.ico ^
     --disable-plugin=multiprocessing ^
-    --file-version="1.0.3" ^
-    --product-version="1.0.3" ^
+    --file-version="1.0.5" ^
+    --product-version="1.0.5" ^
     --windows-file-description="BlueArchive Account tool" ^
     --include-data-dir=platform-tools=platform-tools ^
     --include-data-dir=data/16_9=data/16_9 ^
