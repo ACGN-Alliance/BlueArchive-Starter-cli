684e158b2005da1c6e54765cd6b02e9e32fa064e
MRSlou <79364553+MRSlouzk@users.noreply.github.com>
2023-10-07 20:36:15 +0800

    Update：README

--- pack.bat
+++ pack.bat
@@ -6,8 +6,8 @@ nuitka ^
     --product-name="BlueArchive-Starter" ^
     --windows-icon-from-ico=bas.ico ^
     --disable-plugin=multiprocessing ^
-    --file-version="1.0.6" ^
-    --product-version="1.0.6" ^
+    --file-version="1.0.6.2" ^
+    --product-version="1.0.6.2" ^
     --windows-file-description="BlueArchive Account tool" ^
     --include-data-dir=platform-tools=platform-tools ^
     --include-data-dir=data/16_9=data/16_9 ^
